7458dc05f9cce1f6195390a9cfe341c45b64f48e
homestechjsc <homestechjsc@gmail.com>
2026-08-11 18:45:01 +0700

    Update sw-baocao.js

--- sw-baocao.js
+++ sw-baocao.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'kpi-baocao-v1';
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['/baocao.html'])));
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['./baocao.html'])));
 });
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));

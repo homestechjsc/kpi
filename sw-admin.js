155aae1542761c40e445ccce6e35eb824f308a98
homestechjsc <homestechjsc@gmail.com>
2026-08-11 18:44:52 +0700

    Update sw-admin.js

--- sw-admin.js
+++ sw-admin.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'kpi-admin-v1';
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['/adminmobile.html'])));
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['./adminmobile.html'])));
 });
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));

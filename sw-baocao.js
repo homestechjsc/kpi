e0dbb5d8712881757192367597b756e4feb2833a
homestechjsc <homestechjsc@gmail.com>
2026-08-12 21:48:17 +0700

    Update sw-baocao.js

--- sw-baocao.js
+++ sw-baocao.js
@@ -8,6 +8,13 @@ self.addEventListener('activate', (e) => {
   e.waitUntil(self.clients.claim());
 });
 
+// 👉 BỔ SUNG ĐOẠN NÀY ĐỂ HỖ TRỢ NÚT KIỂM TRA PHIÊN BẢN
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
 // Luôn lấy bản mới nhất từ mạng, nếu offline mới dùng cache cũ
 self.addEventListener('fetch', (e) => {
   e.respondWith(
